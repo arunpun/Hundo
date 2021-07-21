2641bff5da10307843e6ec7d0f03185fbcfbd97d
arunpun <83875939+arunpun@users.noreply.github.com>
2021-07-21 05:20:51 +0100

    Completed functions for Hundo

--- js/site.js
+++ js/site.js
@@ -1,19 +1,48 @@
 //Get values from the page, start function
-function getVaules(){
+function getValues(){
     //Get values
-    let startValue = 
-    //Call generate numbers
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
 
-    //Call display numbers
+    //Convert string to int
+    startValue = parseInt(startValue);
+    endValue = parseInt(endValue);
+
+    //Validation
+    if(Number.isInteger(startValue) && (Number.isInteger(endValue))){
+        //Call generate numbers
+        let numbers = generateNumbers(startValue, endValue);
+        //Call display numbers
+        displayNumbers(numbers);
+    } else {
+        alert("Error: You must enter integers!");
+    }   
 }
 
 //Generate numbers from start value to the end value, logic function
+function generateNumbers(sValue, eValue){ //Brand new variables(parameters)
+    let numbers = []
 
-function generateNumbers(){
-
+    //Get all numbers from start to end
+    //For loop - start, end, increment
+    for(let i = sValue; i <= eValue; i++){
+        numbers.push(i);
+    }
+    return numbers;
 }
 
 //Display the numbers and mark even numbers in bold, view function
-function displayNumbers(){
+function displayNumbers(numbers){
+    //To store table data
+    let templateRows = "";
 
+    //Loop over the array
+    numbers.forEach(element => {
+        if(element % 2 == 0){
+            templateRows += `<tr><td><strong>${element}</strong></td></tr>`;
+        } else {
+            templateRows += `<tr><td>${element}</td></tr>`;
+        }
+    });
+    document.getElementById("results").innerHTML = templateRows;
 }
